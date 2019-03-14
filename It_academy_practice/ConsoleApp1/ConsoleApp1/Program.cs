988da56f26d30fa5ce62efa0ca3e75e941620529
Crystall1time <48089698+Crystall1time@users.noreply.github.com>
2019-03-14 14:30:51 +0300

    Add files via upload

---
+++ It_academy_practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input letter: w,d,s,a ");
+            string letter = Console.ReadLine().ToLower();
+            switch (letter)
+            {
+                case "w":
+                    Console.WriteLine("Move forward");
+                    break;
+                case "d":
+                    Console.WriteLine("Move right");
+                    break;
+                case "s":
+                    Console.WriteLine("Move backward");
+                    break;
+                case "a":
+                    Console.WriteLine("Move left");
+                    break;
+                default:
+                    Console.WriteLine("No need to move");
+                    break;
+            }
+            Console.ReadKey();
+                    }
+    }
+}
---
+++ It_academy_practice/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int number = letters.Length - 1; number >= 0; number--)
+                Console.WriteLine(letters[number]);
+            Console.ReadKey();
+        }
+    }
+}
